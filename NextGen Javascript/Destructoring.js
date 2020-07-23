2d4690a90aa27f9ef433eb2e73c7ee40aacf2d71
rmc3408 <rmc3408@protonmail.com>
2020-07-23 11:27:15 -0400

    s

--- NextGen Javascript/Destructoring.js
+++ NextGen Javascript/Destructoring.js
@@ -1,4 +1,10 @@
-[a, b] = [20, 40];
+[a, b] = [20, 40, 60];
 console.log(a);
 console.log(b);
 
+[a, , c] = [1, 2, 3];
+console.log(a);
+console.log(c);
+
+{ name }  { name: 'Max' };
+console.log(name);
