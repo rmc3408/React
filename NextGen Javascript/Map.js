937f1982f140c0fd3b5b3d823eac33e9a0170ade
rmc3408 <rmc3408@protonmail.com>
2020-07-23 11:53:14 -0400

    e

---
+++ NextGen Javascript/Map.js
@@ -0,0 +1,29 @@
+
+// MAP FUNCTION
+
+// Make run one function for each Array elements.
+
+const num = [1, 2, 3, 4];
+
+const doubleNum = num.map(
+    (num) => { return num * 2; }
+    
+);
+console.log(num);
+console.log(doubleNum);
+
+
+/*
+JavaScript array functions like map() , filter() , reduce().
+
+Particularly important in this course are:
+
+map()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+find()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+findIndex()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+filter()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+reduce()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce?v=b
+concat()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat?v=b
+slice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+splice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+*/
