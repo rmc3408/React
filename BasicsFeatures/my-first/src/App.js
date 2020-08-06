6c0891b8c5c9091a6ac3129ac819148b2cdc901d
Raphael Molinaro Coelho <rmolina9@my.centennialcollege.ca>
2020-08-06 11:16:04 -0400

    b

--- BasicsFeatures/my-first/src/App.js
+++ BasicsFeatures/my-first/src/App.js
@@ -6,6 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
+        <h1> Hello Raphael</h1>
+        
+
+
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
