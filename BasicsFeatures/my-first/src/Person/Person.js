0f839c0b071e461b3dcbb3d8b2b5afca25e5d4f7
Raphael Molinaro Coelho <rmolina9@my.centennialcollege.ca>
2020-08-07 00:17:40 -0400

    b

--- BasicsFeatures/my-first/src/Person/Person.js
+++ BasicsFeatures/my-first/src/Person/Person.js
@@ -3,9 +3,12 @@
 import React from 'react'; //to convert to JSX
 
 const person = (props) => {
-    return (<p> I'm a person called {props.name}!
-     I have {props.age} ,
-     but I look like {Math.floor(Math.random() * 40)} years old! </p>)
+    return (
+        <div>
+            <p> I'm a person called {props.name}! I have {props.age} ,
+     but I look like {Math.floor(Math.random() * 40)} years old! </p>
+            <p> {props.children} = element between open/close tag</p>
+        </div>)
 
 };
 
