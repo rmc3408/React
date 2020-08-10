6950790127cb8a797c33c0a3bb6d314325ebb3d5
Raphael Molinaro Coelho <rmolina9@my.centennialcollege.ca>
2020-08-09 21:16:06 -0400

    c

--- BasicsFeatures/my-first/src/App.js
+++ BasicsFeatures/my-first/src/App.js
@@ -11,12 +11,16 @@ class App extends Component {
       { name="Max", age="29" } ]
   };
 
+  switchNameHandler = () => {
+    console.log("clicked");
+  }
+
   render() {
     return (                      // return JSX, not HTML, () is syntax block.
                                   /* Only allow one block ( <div>  </div>) per render! */
       <div className="App">
         <h1> Hello Raphael </h1>
-        <button> Switch name</button>
+        <button onClick={this.switchNameHandler}> Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} /> 
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
