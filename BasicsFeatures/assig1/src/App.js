42c605b6789bda3c2b3d8f6829d38a35faf2396b
Raphael Molinaro Coelho <rmolina9@my.centennialcollege.ca>
2020-08-10 09:01:25 -0400

    1

--- BasicsFeatures/assig1/src/App.js
+++ BasicsFeatures/assig1/src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Assignment 01 </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          From Udemy React - Basics (section 03), edit <code>src/App.js</code> and save to reload.
         </p>
+
+        
+
       </div>
     );
   }
