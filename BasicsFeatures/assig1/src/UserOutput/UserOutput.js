e1e69ec2ef75d1970876b768486de570dc9d8112
rmc3408 <rmc3408@protonmail.com>
2020-08-11 00:22:03 -0400

    g

--- BasicsFeatures/assig1/src/UserOutput/UserOutput.js
+++ BasicsFeatures/assig1/src/UserOutput/UserOutput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const useroutput = (props) => {
     return (
         <div> 
-            <p> My account is {props.username} </p>
+        <p> My account is {props.username} </p>
         
         
         <p>
