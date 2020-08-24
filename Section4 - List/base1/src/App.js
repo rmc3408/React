a5ac945857771374f98a299abf9e446b4b882450
rmc3408 <rmc3408@protonmail.com>
2020-08-24 11:15:35 -0400

    c

--- Section4 - List/base1/src/App.js
+++ Section4 - List/base1/src/App.js
@@ -52,23 +52,23 @@ class App extends Component {
           <p>List and Conditionals</p>
       </header>
         
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        
+             
         <button 
           style={style}
           onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+        <div> 
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age} />
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Raphael')}
+              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+            <Person 
+              name={this.state.persons[2].name} 
+                age={this.state.persons[2].age} />
+        </div>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
