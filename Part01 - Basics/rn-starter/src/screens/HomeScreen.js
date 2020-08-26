f6b7e216e25c8086b2244d289d2f966f7f18e698
rmc3408 <rmc3408@protonmail.com>
2020-08-26 16:59:58 -0400

    d

--- Part01 - Basics/rn-starter/src/screens/HomeScreen.js
+++ Part01 - Basics/rn-starter/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>My Home Screen</Text>;
+  return <Text style={styles.text}>My Raph's Home Screen</Text>;
 };
 
 const styles = StyleSheet.create({
