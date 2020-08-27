33765109c789273a8aeac6adb48ffe97c39d312c
rmc3408 <rmc3408@protonmail.com>
2020-08-27 19:52:14 -0400

    e

---
+++ Block03 - Props/Personal.js
@@ -0,0 +1,10 @@
+class Personal extends React.Component {
+  render() {
+    return (
+      <div>
+			  <h1> Email From {P.} to </h1>
+        <br>
+      </div>
+    );
+  }
+}
---
+++ Block03 - Props/index.js
@@ -0,0 +1,13 @@
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Personal from="Raphael" to="Ivan"/>
+        
+      </div >
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
