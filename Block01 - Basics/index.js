393e68d5575c346fc44015c5752ac41559a206f4
rmc3408 <rmc3408@protonmail.com>
2020-08-27 12:22:19 -0400

    b

---
+++ Block01 - Basics/index.js
@@ -0,0 +1,12 @@
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+			<h1>Hello Raphael</h1>
+			<h1>Hello Raphael</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
