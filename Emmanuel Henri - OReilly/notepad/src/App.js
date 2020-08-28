0c9f98cf72a1ca15a7c356945104fd10a815fb03
rmc3408 <rmc3408@protonmail.com>
2020-08-28 10:35:18 -0400

    notepad

--- Emmanuel Henri - OReilly/notepad/src/App.js
+++ Emmanuel Henri - OReilly/notepad/src/App.js
@@ -6,19 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 className="App-title">Notepad Application </h2>
+          <p> Building a React App from Scratch - by Emmanuel Henri - O'Reilly</p>
       </header>
+
     </div>
   );
 }
