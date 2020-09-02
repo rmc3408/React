e02f17f19ee69c05d00b39641dc6441f1cce3e5c
rmc3408 <rmc3408@protonmail.com>
2020-09-01 22:08:50 -0400

    i

--- Block03 - Props/App.js
+++ Block03 - Props/App.js
@@ -1,14 +1,34 @@
-
 class App extends React.Component {
-
   render() {
+    console.log(this.state);
+
     return (
       <div>
-        <Friend name="Emma" hobbies={["Piano", "ballet", "Dolls"]} />
-        <Friend name="Ivan" hobbies={["Docs", "Books", "kisses"]} />
-        <Friend hobbies={["Play", "Run", "Sleep"]} bangs={4}/>
-        
-        
+        <div>
+          <Personal from="Raphael" to="Ivan" />
+          <Personal from="Andrea" to="Raphael" />
+
+          <Personal
+            from="Andrea"
+            to="Alessandro"
+            num={3}
+            data={[1, 2, 3, 4, 5]}
+            isFunny={true}
+            bangs={4}
+            image=".\fire_1f525.png"
+          />
+        </div>
+
+        <div>
+          <Friend name="Emma" hobbies={["Piano", "ballet", "Dolls"]} />
+          <Friend name="Ivan" hobbies={["Docs", "Books", "kisses"]} />
+          <Friend hobbies={["Play", "Run", "Sleep"]} bangs={4} />
+        </div>
+
+        <div className='Slot'>
+          <h1> Slot Machine </h1>
+          <Slot frnd1="soup" frnd2="tree" frnd3="apple" num={3} />
+        </div>
       </div>
     );
   }
