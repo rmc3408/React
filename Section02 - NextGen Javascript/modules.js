a020161963130cf41f2ea048c0cc1661d90813fe
rmc3408 <rmc3408@protonmail.com>
2020-09-07 10:34:13 -0400

    a

---
+++ Section02 - NextGen Javascript/modules.js
@@ -0,0 +1,26 @@
+function help() {
+    console.log('I did help it');
+}
+
+function sort() {
+    console.log('I will separate');
+}
+
+function sing() {
+    console.log('I can sing');
+}
+
+export default help; //export only helpfull function
+export { sort, sing }; //export selected items or function
+
+import help, { sort } from './modules';
+
+
+const foo = 10;
+
+export default foo; //export the constant foo
+
+
+// On the target file
+import foo from './boo.js';
+import { foo } from './boo.js';
