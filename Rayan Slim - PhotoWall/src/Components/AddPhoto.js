eb75e0bdd9a8838eae740109de3802bf782cdf08
rmc3408 <rmc3408@protonmail.com>
2020-12-15 17:06:32 -0500

    router doner

--- Rayan Slim - PhotoWall/src/Components/AddPhoto.js
+++ Rayan Slim - PhotoWall/src/Components/AddPhoto.js
@@ -4,7 +4,16 @@ import React, { Component } from 'react';
 
 class AddPhoto extends Component {
     render() {
-      return <h1> Upload </h1>
+        return (<div> 
+            <h2> PhotoWall </h2>
+            <h5> Upload your picture</h5>
+            <div className="forma"> 
+                <form>
+                    <input type="text" placeholder="link.." />
+                    <input type="text" placeholder="description" />
+                </form>
+            </div>
+          </div>)
     }
 }
 export default AddPhoto;
