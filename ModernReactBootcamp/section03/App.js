2410c13b9a7362eccb886049f021f2e12d840cde
rmc3408 <rmc3408@protonmail.com>
2020-12-22 22:45:54 -0500

    section03 half

--- ModernReactBootcamp/section03/App.js
+++ ModernReactBootcamp/section03/App.js
@@ -1,35 +1,38 @@
 class App extends React.Component {
   render() {
-    console.log(this.state);
+    //console.log(this.state);
     
-
     return (
       <div>
         <div>
           <Personal from="Raphael" to="Ivan"/>
           <Personal from="Andrea" to="Raphael"/>
 
-          <Personal
+          <Personal //type of pros ==> pass using {} or ""
             from="Andrea"
             to="Alessandro"
+            numberString="3"
             num={3}
             data={[1, 2, 3, 4, 5]}
             isFunny={true}
             bangs={4}
             image=".\fire_1f525.png"
+            objeto={{ name: "Raph", age: 39 } }
           />
         </div>
 
+        <div className='Slot'>
+          <h1> Slot Machine </h1>
+          <Slot frnd1="soup" frnd2="tree" frnd3="apple" num={3} /> 
+        </div> 
+        
         <div style={{fontSize:'20px'}}>
           <Friend name="Emma" hobbies={["Piano", "ballet", "Dolls"]} />
           <Friend name="Ivan" hobbies={["Docs", "Books", "kisses"]} />
           <Friend hobbies={["Play", "Run", "Sleep"]} bangs={4} />
         </div>
 
-        <div className='Slot'>
-          <h1> Slot Machine </h1>
-          <Slot frnd1="soup" frnd2="tree" frnd3="apple" num={3} />
-        </div>
+        
       </div>
     );
   }
--- ModernReactBootcamp/section03/Personal.js
+++ ModernReactBootcamp/section03/Personal.js
@@ -1,14 +1,12 @@
 class Personal extends React.Component {
-  
-
   render() {
-    console.log(this.props);
-    let bangs = '!'.repeat(this.props.bangs);
+    console.log(this.props); //cannot change or add data into PROPS (properties)
+    const theBangs = '!'.repeat(this.props.bangs);
 
     return (
       <div>
-        <h1> Hello from {this.props.from} to {this.props.to} {bangs}</h1>
-        <img src={this.props.image}/>
+        <h1> Hello from {this.props.from} to {this.props.to} {theBangs}</h1>
+        <img src={this.props.image} alt="fire" />
         
       </div>
     );
