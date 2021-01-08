c1c6b0a36e8cff88a02dbcabaaa5a51b3fef9507
rmc3408 <rmc3408@protonmail.com>
2021-01-07 21:05:03 -0500

    con flipper done

--- ModernReactBootcamp/section09/coin-flipper/src/components/Coin.js
+++ ModernReactBootcamp/section09/coin-flipper/src/components/Coin.js
@@ -4,7 +4,9 @@ class Coin extends Component {
   render() {
     return (
       <div>
-        {this.props.img == null ? 'Start to Play' : <img src={this.props.img.imgSrc} alt='coin'/>}
+        {this.props.img && <img src={this.props.img.imgSrc} alt='coin'/>}
+
+        {/* {this.props.img == null ? 'Start to Play' : <img src={this.props.img.imgSrc} alt='coin'/>} */}
       </div>
     )
   }
