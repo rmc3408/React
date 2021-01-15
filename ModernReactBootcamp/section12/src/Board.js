f92255681500e47d0b1603cc78755b850226cbdb
rmc3408 <rmc3408@protonmail.com>
2021-01-14 20:03:20 -0500

    Game complete

--- ModernReactBootcamp/section12/src/Board.js
+++ ModernReactBootcamp/section12/src/Board.js
@@ -4,8 +4,8 @@ import "./Board.css";
 
 class Board extends Component {
   static defaultProps = {
-    nRows: 3,
-    nCols: 3,
+    nRows: 4,
+    nCols: 5,
     chanceLightStartsOn: 0.35,
   };
 
@@ -30,7 +30,6 @@ class Board extends Component {
     return board;
   }
 
-
   flipCellsAroundMe(coord) {
     let { nCols, nRows } = this.props;
     let board = this.state.board;
@@ -51,14 +50,13 @@ class Board extends Component {
     flipCell(r + 1, c);
     flipCell(r, c - 1);
     flipCell(r, c + 1);
-    
 
     //let test = [false,false,true].every(p => p === false)
     //console.log(test);
-    let colsWon = board.map(col => {
-       return col.every(c => c === false);
+    let colsWon = board.map((col) => {
+      return col.every((c) => c === false);
     });
-    let won = colsWon.every(row => row === true);
+    let won = colsWon.every((row) => row === true);
     console.log(won);
 
     this.setState({
@@ -71,30 +69,44 @@ class Board extends Component {
 
   render() {
     if (this.state.isWinner) {
-      return <h1> WON! </h1>
+      return (
+        <div>
+          <div className="Board-Win">
+            <div className="neon-blue">You</div>
+            <div className="neon-orange">Won</div>
+          </div>
+        </div>
+      );
     }
     return (
       <div>
-        <h1> Lights out</h1>
-        <table className="Board">
-          <tbody>
-            {this.state.board.map((row, idxRow) => {
-              return (
-                <tr key={idxRow}>
-                  {row.map((val, idxCol) => (
-                    // <Cell key={idxCol} isLit={this.state.board[idxRow][idxCol]} />
-                    <Cell
-                      key={`${idxRow}-${idxCol}`}
-                      isLit={val}
-                      flipCells={() => this.flipCellsAroundMe(`${idxRow}-${idxCol}`)
-                      }
-                    />
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className="Board-title">
+          <div className="neon-blue">Lights</div>
+          <div className="neon-orange">Out</div>
+        </div>
+
+        <div>
+          <table className="Board">
+            <tbody>
+              {this.state.board.map((row, idxRow) => {
+                return (
+                  <tr key={idxRow}>
+                    {row.map((val, idxCol) => (
+                      // <Cell key={idxCol} isLit={this.state.board[idxRow][idxCol]} />
+                      <Cell
+                        key={`${idxRow}-${idxCol}`}
+                        isLit={val}
+                        flipCells={() =>
+                          this.flipCellsAroundMe(`${idxRow}-${idxCol}`)
+                        }
+                      />
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
     // if the game is won, just show a winning msg & render nothing else
