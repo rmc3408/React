f73869e09d5763325001eecd0844655f9d54d94e
rmc3408 <rmc3408@protonmail.com>
2021-01-21 20:41:09 -0500

    video 3 done

--- ModernReactBootcamp/section16/src/Die.js
+++ ModernReactBootcamp/section16/src/Die.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props) {
+    super(props);
+    this.sendClick = this.myOnClick.bind(this);
+  }
+  myOnClick() {
+    this.props.handleClick(this.props.idx);
+  }
+
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
+        onClick={this.sendClick}
+      >{this.props.val}</button>
     );
   }
 }
