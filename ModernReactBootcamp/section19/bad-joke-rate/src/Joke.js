6c95b685d5538371c0d58b55a0a2c8763ab13853
rmc3408 <rmc3408@protonmail.com>
2021-01-27 17:02:52 -0500

    video 6 done

--- ModernReactBootcamp/section19/bad-joke-rate/src/Joke.js
+++ ModernReactBootcamp/section19/bad-joke-rate/src/Joke.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './Joke.css';
 
 class Joke extends Component {
+    
+    handleUp(delta) {
+        this.props.voteUP(this.props.id, delta);
+    }
+
     render() {
+        const face = this.props.vote >= 6 ? 4 :
+            (this.props.vote >= 3 ? 3 : (this.props.vote > -3 ? 2 :
+                (this.props.vote > -6 ? 1 : 0)));
+        const mood = this.props.vote >= 6 ? 'green' :
+                (this.props.vote >= 3 ? 'yellow' : (this.props.vote > -3 ? 'azure' :
+                    (this.props.vote > -6 ? 'pink' : 'red')));
         return (
-            <div>
-                {this.props.joke}
-            
+            <div className="Joke">
+                <div className="Joke-buttons">
+                    <i className="fas fa-arrow-up" onClick={() => this.handleUp(1)}></i>
+                    <span style={{border: `3px solid ${mood}`}} className="Joke-votes">{this.props.vote}</span>
+                    <i className="fas fa-arrow-down" onClick={() => this.handleUp(-1)}></i>
+                </div>
+                <div className="Joke-text">{this.props.joke}</div>
+                <div className="Joke-smiley"><i class={`em ${this.props.emoji[face]}`} aria-label="BIRD"></i></div>
             </div>
         )
     }
--- ModernReactBootcamp/section19/bad-joke-rate/src/JokeList.js
+++ ModernReactBootcamp/section19/bad-joke-rate/src/JokeList.js
@@ -1,43 +1,74 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Joke from './Joke';
-import './JokeList.css';
+import Joke from "./Joke";
+import "./JokeList.css";
 
 class JokeList extends Component {
-    static defaultProps = {
-        numJokes: 10,
-    };
+  static defaultProps = {
+      numJokes: 10,
+      emoji: [
+          "em-nauseated_face",
+          "em-disappointed_relieved",
+          "em-neutral_face",
+          "em-smiley",
+          "em-rolling_on_the_floor_laughing"]
+  };
   constructor(props) {
     super(props);
-      this.state = {
-        jokes: [],
-        isFullLoaded: false,
+    this.state = {
+      jokes: [],
+      isFullLoaded: false,
     };
+    this.voteUP = this.voteUP.bind(this);
   }
-    async componentDidMount() {
-        while(this.state.jokes.length < this.props.numJokes) {
-            let joke = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: 'application/json'}});
-            //console.log(joke.data.id);
-            this.setState(st => ({
-                jokes: [...st.jokes, joke.data ],
-                isFullLoaded: true,
-            }));
-        }
+  async componentDidMount() {
+    while (this.state.jokes.length < this.props.numJokes) {
+      let joke = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      });
+      //console.log(joke.data.id);
+      joke.data.vote = 0;
+      this.setState((st) => ({
+        jokes: [...st.jokes, joke.data],
+        isFullLoaded: true,
+      }));
+    }
+  }
+  voteUP(id, delta) {
+    this.setState((st) => ({
+      jokes: [...st.jokes.map((j) =>
+          j.id === id ? { ...j, vote: j.vote + delta } : { ...j }
+        ),
+      ],
+    }));
   }
 
-    render() {
-        const mappedJokes = this.state.jokes.map(j => <Joke key={j.id} joke={j.joke}/> )
-        return (
-            <div className='JokeList'>
-                <div className="JokeList-sidebar">
-                    <h1 className="JokeList-title"><span>Daddy</span> Jokes</h1>
-                    <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/230/grinning-face-with-one-large-and-one-small-eye_1f92a.png" alt='emoji' />
-                    <button className="JokeList-btn">New jokes</button>
-                </div>
-                <div className="JokeList-jokes">
-                    {mappedJokes}
-                </div>
-            </div>);
+  render() {
+    const mappedJokes = this.state.jokes.map((j) => (
+      <Joke
+        key={j.id}
+        id={j.id}
+        joke={j.joke}
+        vote={j.vote}
+        emoji={this.props.emoji}    
+        voteUP={this.voteUP}
+      />
+    ));
+    return (
+      <div className="JokeList">
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Daddy</span> Jokes
+          </h1>
+          <img
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/230/grinning-face-with-one-large-and-one-small-eye_1f92a.png"
+            alt="emoji"
+          />
+          <button className="JokeList-btn">New jokes</button>
+        </div>
+        <div className="JokeList-jokes">{mappedJokes}</div>
+      </div>
+    );
   }
 }
 
