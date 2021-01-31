13a9e868f26c32cba5f328cc34ae411b7b43260c
rmc3408 <rmc3408@protonmail.com>
2021-01-31 17:15:56 -0500

    NavLink router

--- ModernReactBootcamp/section20/routed/src/App.js
+++ ModernReactBootcamp/section20/routed/src/App.js
@@ -1,20 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import Dog from './Dog';
 import Bird from './Bird';
 import Cat from './Cat';
 
 function App() {
+  const Animals = () => <h1>PET ZOO</h1>;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React Router</p>
       </header>
+      <nav className='App-nav'>
+        <a href="/">Old link Home</a>
+        <Link to='/'>Home</Link>
+        <Link to='/cat'>Cat</Link>
+        <Link to='/dog'>Dog</Link>
+        <Link to='/bird'>Bird</Link>
+
+        <NavLink exact activeClassName='App-link-active' to='/'>Home</NavLink>
+        <NavLink activeClassName='App-link-active' to='/cat'>Cat</NavLink>
+        <NavLink activeClassName='App-link-active' to='/dog'>Dog</NavLink>
+        <NavLink activeClassName='App-link-active' to='/bird'>Bird</NavLink> 
+      </nav>
       <div className='router'>
-        <div className='box'><Dog /> </div>
-        <div className='box'><Bird /> </div>
-        <div className='box'><Cat /> </div>
+        <Switch>
+            <Route exact path='/dog' component={Dog} />
+            <Route exact path='/bird' component={Bird} />
+            <Route exact path='/cat' component={Cat} />
+            <Route exact path='/' component={Animals} />
+        </Switch>
+        
         
       </div>
     </div>
--- ModernReactBootcamp/section20/routed/src/Dog.js
+++ ModernReactBootcamp/section20/routed/src/Dog.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Dog() {
-    return <img src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/273/dog_1f415.png' alt='dog' />
-
+  return <div className="box">
+    <img
+      src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/273/dog_1f415.png"
+      alt="dog"
+    />
+  </div>;
 }
 
 export default Dog;
