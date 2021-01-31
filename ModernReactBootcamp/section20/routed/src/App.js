3a53b3e662db1c954768b5679e72db98fb83bd2d
rmc3408 <rmc3408@protonmail.com>
2021-01-31 17:37:23 -0500

    section 20 done

--- ModernReactBootcamp/section20/routed/src/App.js
+++ ModernReactBootcamp/section20/routed/src/App.js
@@ -24,13 +24,26 @@ function App() {
         <NavLink activeClassName='App-link-active' to='/cat'>Cat</NavLink>
         <NavLink activeClassName='App-link-active' to='/dog'>Dog</NavLink>
         <NavLink activeClassName='App-link-active' to='/bird'>Bird</NavLink> 
+
+        <NavLink activeClassName='App-link-active' to='/dog/r'>Dog render </NavLink>
+        <NavLink activeClassName='App-link-active' to='/dog/c'>Dog create </NavLink> 
+
+
       </nav>
       <div className='router'>
         <Switch>
-            <Route exact path='/dog' component={Dog} />
-            <Route exact path='/bird' component={Bird} />
-            <Route exact path='/cat' component={Cat} />
-            <Route exact path='/' component={Animals} />
+          <Route exact path='/dog' component={Dog} />
+          <Route exact path='/bird' component={Bird} />
+          <Route exact path='/cat' component={Cat} />
+          <Route exact path='/' component={Animals} />
+
+          
+          {/*  create new component (render, didMount, WillUnmount) */}
+          <Route exact path='/dog/c' component={()=> <Dog name="Muffin"/>} />
+          {/*  Render component */}
+          <Route exact path='/dog/r' render={()=> <Dog name="Cookie"/>} />
+
+
         </Switch>
         
         
--- ModernReactBootcamp/section20/routed/src/Dog.js
+++ ModernReactBootcamp/section20/routed/src/Dog.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-function Dog() {
+function Dog(props) {
   return <div className="box">
+    <h2>My name is {props.name}</h2>
     <img
       src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/273/dog_1f415.png"
       alt="dog"
