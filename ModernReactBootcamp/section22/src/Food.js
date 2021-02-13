835e164b7ec943c21564021cb0c1740c1f6f24f5
rmc3408 <rmc3408@protonmail.com>
2021-02-12 19:17:04 -0500

    video 5

--- ModernReactBootcamp/section22/src/Food.js
+++ ModernReactBootcamp/section22/src/Food.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Food extends Component {
     render() {
         const { name } = this.props.match.params;
-        const url = `https://source.unsplash.com/1600x900/?${name}`;     
+        const url = `https://source.unsplash.com/1600x900/?${name}`;
+        const checking = /\d/.test(name);
         return (
             <div>
-                <h1>I love to eat {name}</h1>
-                <img src={url} alt={name} />
+                { checking ? <Redirect to='/' /> : (<div>
+                    <h1>I love to eat {name}</h1>
+                    <img src={url} alt={name} />
+                </div>)
+            }
             </div>
+            
         )
     }
 }
