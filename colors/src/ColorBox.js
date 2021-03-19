c6c48b434c3d4f76975353e1e84c1baa355e46b3
rmc3408 <rmc3408@protonmail.com>
2021-03-18 22:21:40 -0400

    Add overlay Animation

--- colors/src/ColorBox.js
+++ colors/src/ColorBox.js
@@ -1,19 +1,57 @@
-import React, { Component } from 'react';
-import './ColorBox.css';
+import React, { Component } from "react";
+import "./ColorBox.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 class ColorBox extends Component {
-    render() {
-        return (
-            <div style={{ backgroundColor: this.props.background }} className='ColorBox'>
-                <div className='copy-container'>
-                    <div className='box-content'><span>{this.props.name}</span></div>
-                    <button className='copy-button'>Copy</button>
-                </div>
-                <span className='see-more'>MORE</span>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCopied: false,
+    };
+    this.copyEffect = this.copyEffect.bind(this);
+  }
 
+  copyEffect() {
+    this.setState(
+      {
+        isCopied: true,
+      },
+      () => {
+        setTimeout(() => {
+          this.setState({
+            isCopied: false,
+          });
+        }, 1600);
+      }
+    );
+  }
+
+  render() {
+    const { background } = this.props;
+    const { isCopied } = this.state;
+    return (
+      <CopyToClipboard text={this.props.background} onCopy={this.copyEffect}>
+        <div style={{ backgroundColor: background }} className="ColorBox">
+          <div
+            className={`copy-overlay ${isCopied && "show"}`}
+            style={{ backgroundColor: background }}
+          />
+            <div className={`copied-msg ${isCopied && "show"}`}>
+              <h1>COPIED</h1>
+              <p>{background}</p>
             </div>
-        )
-    }
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{this.props.name}</span>
+            </div>
+
+            <button className="copy-button">Copy</button>
+          </div>
+          <span className="see-more">MORE</span>
+        </div>
+      </CopyToClipboard>
+    );
+  }
 }
 
-export default ColorBox
+export default ColorBox;
